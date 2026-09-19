2e29045c49f1a7585083456ba2bc5d4dff54061c
Brave290 <akanjimusab@gmail.com>
2026-09-19 18:36:35 +0100

    fix: import Database from @turna/types instead of subpath

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,2 +1,2 @@
 export { createServerClient, createBrowserClient, createServiceClient } from './client';
-export type { Database } from '@turna/types/database';
+export type { Database } from '@turna/types';
